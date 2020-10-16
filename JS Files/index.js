4a3554968dda662fc4af9e35b3d54491dc53a43e
sa-mh <62239021+sa-mh@users.noreply.github.com>
2020-10-16 10:33:24 +0100

    Initial commit

---
+++ JS Files/index.js
@@ -0,0 +1,172 @@
+"use strict";
+const titlei = document.getElementById("bookTitle");
+const authori = document.getElementById("bookAuthor");
+const bStatusi = document.getElementById("bookStatus");
+const genrei = document.getElementById("bookGenre");
+const againi = document.getElementById("bookReadAgain");
+const pagesi = document.getElementById("bookPages");
+const ratingi = document.getElementById("bookRating");
+const isbni = document.getElementById("bookISBN");
+const addButton = document.getElementById("add");
+const removeButton = document.getElementById("remove");
+const saveButton = document.getElementById("save");
+const theInputs = document.getElementById("inputLand");
+const bookOutput = document.getElementById("imageDiv");
+const slideList = document.getElementById("slideList");
+const lButt = document.getElementById("leftButton");
+const rButt = document.getElementById("rightButton");
+const fields = document.querySelectorAll("div.centered table input, div.centered table select");
+
+console.log(theInputs);
+
+theInputs.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const data = {
+        title: titlei.value,
+        author: authori.value,
+        genre: genrei.value,
+        status: bStatusi.value,
+        pages: pagesi.value,
+        rating: ratingi.value,
+        isbn: isbni.value,
+        coverURL: "bookCover.jpg"
+    }
+    console.log(data);
+    fetch("http://localhost:8080/addbook", { //Make request
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    }).then(response => { // Receive response
+        return response.json(); // Convert response body to json
+    }).then(data => { //json data from previous .then()
+        addCards();
+        console.log(data);
+        console.log("got here");
+        this.reset();
+    }).catch(error => console.log(error));
+    clearValues();
+    disableEdit();
+});
+function addCards() {
+    let i = 0;
+    bookOutput.innerHTML = ' ';
+    slideList.innerHTML = ' ';
+    fetch("http://localhost:8080/getbooks").then(response => response.json()).then(arrayOfBooks => {
+        console.log("Books: ", arrayOfBooks);
+        bookOutput.innerHTML = ' ';
+        arrayOfBooks.forEach(function (book) {
+            console.log(book);
+            const slideInfo = document.createElement("li");
+            slideInfo.setAttribute("data-target", "#demo");
+            slideInfo.setAttribute("data-slide-to", i);
+            if (i == 0) {
+                slideInfo.setAttribute("class", "active");
+            }
+            slideInfo.setAttribute("data-id", book.id)
+            slideInfo.setAttribute("onClick", "changeBook()");
+            slideList.appendChild(slideInfo);
+
+
+            const slide = document.createElement("div");
+            if (i == 0) {
+                slide.className = "carousel-item active";
+            }
+            else {
+                slide.className = "carousel-item";
+            }
+            const bookCover = document.createElement("img");
+            bookCover.setAttribute("src", "bookCover.jpg");
+            bookCover.setAttribute("width", 275);
+            bookCover.setAttribute("height", 370);
+            bookCover.setAttribute("alt", "b" + book.id);
+
+            slide.appendChild(bookCover);
+
+            bookOutput.appendChild(slide);
+            if (i == 0) {
+                titlei.value = book.title;
+                authori.value = book.author;
+                genrei.value = book.genre;
+                bStatusi.value = book.status;
+                pagesi.value = book.pages;
+                ratingi.value = book.rating;
+                isbni.value = book.isbn;
+                disableEdit();
+            }
+
+
+            i++;
+        });
+    }).catch(error => console.error(error));
+}
+addCards();
+lButt.addEventListener("click", function (event) {
+    changeBook();
+});
+rButt.addEventListener("click", function (event) {
+    changeBook();
+});
+function removeBook() {
+    let listItem = document.querySelector("li.active");
+    let imageList = document.querySelector("div.active");
+    fetch("http://localhost:8080/remove/" + listItem.getAttribute("data-id").toString(), { //Make request
+        method: "DELETE"
+    })
+        .catch(error => console.log(error));
+    location.reload();
+}
+function changeBook() {
+    setTimeout(() => {  console.log("World!"); 
+    enableEdit();
+    var listItem = document.querySelector("li.active");
+    console.log("http://localhost:8080/getbook/" + listItem.getAttribute("data-id"));
+    fetch("http://localhost:8080/getbook/" + listItem.getAttribute("data-id"))
+        .then(response => response.json())
+        .then(receivedData => {
+            titlei.value = receivedData.title;
+            authori.value = receivedData.author;
+            genrei.value = receivedData.genre;
+            bStatusi.value = receivedData.status;
+            pagesi.value = receivedData.pages;
+            ratingi.value = receivedData.rating;
+            isbni.value = receivedData.isbn;
+        })
+        .catch(error => console.error(error));
+    disableEdit();}, 600);
+}
+function updateBook(){
+    const data = {
+        title: titlei.value,
+        author: authori.value,
+        genre: genrei.value,
+        status: bStatusi.value,
+        pages: pagesi.value,
+        rating: ratingi.value,
+        isbn: isbni.value,
+        coverURL: "bookCover.jpg"
+    }
+    console.log(data);
+    fetch("http://localhost:8080/addbook", { //Make request
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    }).then(response => { // Receive response
+        return response.json(); // Convert response body to json
+    }).then(data => { //json data from previous .then()
+        addCards();
+        console.log(data);
+        console.log("got here");
+        this.reset();
+    }).catch(error => console.log(error));
+    clearValues();
+    disableEdit();
+}
+
+//doesnt always seem to delete the right thing
+//https://github.com/JHarry444/SpringDucks/tree/master/src/main/java/com/qa/duck/service
+//https://github.com/JHarry444/CN-FE/blob/main/07_example_site/index.html
+//update?id=id
---
+++ JS Files/mainButtons.js
@@ -0,0 +1,77 @@
+"use strict";
+
+
+const uButton = document.getElementById("update");
+const aButton = document.getElementById("add");
+const cButton = document.getElementById("clear");
+const rButton = document.getElementById("remove");
+const sButton = document.getElementById("save");
+const canButton = document.getElementById("cancel");
+
+const mB = document.getElementById("changesButts");
+
+const title = document.getElementById("bookTitle");
+const author = document.getElementById("bookAuthor");
+const bStatus = document.getElementById("bookStatus");
+const genre = document.getElementById("bookGenre");
+const again = document.getElementById("bookReadAgain");
+const pages = document.getElementById("bookPages");
+const rating = document.getElementById("bookRating");
+const isbn = document.getElementById("bookISBN");
+
+//document.querySelectorAll("div.centered table input, div.centered table select");
+
+function startEdit() {
+    uButton.style.display = "none";
+    mB.style.display = "block";
+    aButton.style.display = "none";
+    cButton.style.display = "none";
+    rButton.style.display = "none";
+    sButton.style.display = "inline-block";
+    canButton.style.display = "inline-block";
+    enableEdit();
+}
+
+function finishEdit() {
+    uButton.style.display = "inline-block";
+    mB.style.display = "none";
+    aButton.style.display = "inline-block";
+    cButton.style.display = "inline-block";
+    rButton.style.display = "inline-block";
+    sButton.style.display = "none";
+    canButton.style.display = "none";
+    disableEdit();
+}
+function disableEdit() {
+    title.disabled = true;
+    author.disabled = true;
+    bStatus.disabled = true;
+    genre.disabled = true;
+    again.disabled = true;
+    pages.disabled = true;
+    rating.disabled = true;
+    isbn.disabled = true;
+
+}
+function enableEdit() {
+    title.disabled = false;
+    author.disabled = false;
+    bStatus.disabled = false;
+    genre.disabled = false;
+    again.disabled = false;
+    pages.disabled = false;
+    rating.disabled = false;
+    isbn.disabled = false;
+
+}
+function clearValues() {
+    title.value = null;
+    author.value = null;
+    bStatus.selectedIndex = 0;
+    genre.selectedIndex = 0;
+    again.selectedIndex = 0;
+    pages.value = null;
+    rating.value = null;
+    isbn.value = null;
+    enableEdit();
+}
